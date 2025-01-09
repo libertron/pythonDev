852297a7358d569cfccd5534c9e5edf33cd93190
libertron <marcusserginio96@gmail.com>
2025-01-09 13:16:21 -0500

    variable name declaration

--- Basics/project2.py
+++ Basics/project2.py
@@ -1,4 +1,31 @@
+# Variable declaration
 x = 5
 y = "John"
 print(x)
 print(y)
+
+# Casting the variable
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+#show type of variable
+print(type(x))
+
+# x="John" is the same as x='John'
+
+# Variable name are case-sensitive
+a = 4
+A = "Sally"
+print(a,A)
+
+# variable declaration format
+# camel case
+myVariableName = "value"
+# pascal case
+MyVariableName = "value"
+# snake case
+my_variable_name = "value"
