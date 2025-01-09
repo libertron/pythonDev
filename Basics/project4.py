2d67b8352be598177a4bd383c95bbc4a10d22807
libertron <marcusserginio96@gmail.com>
2025-01-09 16:52:26 -0500

    Output in python

---
+++ Basics/project4.py
@@ -0,0 +1,22 @@
+# Python output system
+x = "Python"
+y = "is"
+z = "awesome"
+
+# Print separately, also works for different data types
+print(x,y,z)
+
+# + operator for string is concatenation
+print(x + y + z)
+
+# + for number is a mathematical operator (addition to be clear)
+a = 5
+b = 10
+
+print (a + b)
+
+# using + to concatenate string and number won't work (it'll give error)
+# print(a + x) will give error
+
+# best way to output different data type together is by using comma (,)
+print(x, y, a)
